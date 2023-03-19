110f81cffab0a3b93e343355fe07ea02c1d35576
Vinícius <viniscrv@gmail.com>
2023-03-19 16:10:28 -0300

    feat: hash de senha em criação de usuário

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { prisma } from "@/lib/prisma";
 import { z } from "zod";
+import { hash } from "bcryptjs";
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
     const registerBodySchema = z.object({
@@ -11,11 +12,23 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 
     const { name, email, password } = registerBodySchema.parse(req.body);
 
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (userWithSameEmail) {
+        return res.status(409).send();
+    }
+
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password,
+            password_hash,
         },
     });
 
