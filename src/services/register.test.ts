b9570cf608f058fee515e1f2e64f31354fee840a
Vinícius <viniscrv@gmail.com>
2023-03-27 14:04:33 -0300

    feat: criado Service de autenticação

--- src/services/register.test.ts
+++ src/services/register.test.ts
@@ -50,7 +50,7 @@ describe("Register Service", () => {
             password: "123456"
         });
 
-        expect(() =>
+        await expect(() =>
             registerService.execute({
                 name: "John Doe",
                 email,
