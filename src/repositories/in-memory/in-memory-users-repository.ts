0a5299481842d2a322ff24455ff4a3263baf92a0
Vinícius <viniscrv@gmail.com>
2023-03-28 13:19:26 -0300

    feat: criado Service de perfil

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -5,6 +5,16 @@ export class InMemoryUsersRepository implements UsersRepository {
 
     public items: User[] = [];
 
+    async findById(id: string) {
+        const user = this.items.find(item => item.id === id);
+
+        if (!user) {
+            return null;
+        }
+
+        return user;
+    }
+
     async findByEmail(email: string) {
         const user = this.items.find(item => item.email === email);
 
@@ -27,5 +37,7 @@ export class InMemoryUsersRepository implements UsersRepository {
         this.items.push(user);
         return user;
     }
+
+
 }
 
