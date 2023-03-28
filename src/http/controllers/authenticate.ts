1581bb9fa5fcab3daac0a892f4c254c52ac9ad78
Vinícius <viniscrv@gmail.com>
2023-03-28 13:07:12 -0300

    refactor: factory pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
-import { AuthenticateService } from "@/services/authenticate";
 import { InvalidCredentialsError } from "@/services/errors/invalid-credentials-error";
+import { makeAuthenticateService } from "@/services/factories/make-authenticate-service";
 
 export async function authenticate(req: FastifyRequest, res: FastifyReply) {
     const authenticateBodySchema = z.object({
@@ -13,8 +12,7 @@ export async function authenticate(req: FastifyRequest, res: FastifyReply) {
     const { email, password } = authenticateBodySchema.parse(req.body);
 
     try {
-        const usersRepository = new PrismaUsersRepository();
-        const authenticateService = new AuthenticateService(usersRepository);
+        const authenticateService = makeAuthenticateService();
 
         await authenticateService.execute({ email, password });
     } catch (err) {
