6e0080c475ac7ac5b64bffd02d1ec4a1d7ca66d1
Vinícius <viniscrv@gmail.com>
2023-04-02 17:49:17 -0300

    feat: validado horários entre check-ins em Service de validação de check-in

--- src/services/validate-check-in.test.ts
+++ src/services/validate-check-in.test.ts
@@ -1,7 +1,8 @@
-import { expect, describe, test, beforeEach, afterEach } from "vitest";
+import { expect, describe, test, beforeEach, afterEach, vi } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { ValidateCheckInService } from "@/services/validate-check-in";
 import { ResourceNotFoundError } from "@/services/errors/resource-not-found-error";
+import { LateCheckInValidationError } from "@/services/errors/late-check-in-validation-error";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInService;
@@ -11,11 +12,11 @@ describe("Validate Check-in Service", () => {
         checkInsRepository = new InMemoryCheckInsRepository();
         sut = new ValidateCheckInService(checkInsRepository);
 
-        // vi.useFakeTimers();
+        vi.useFakeTimers();
     });
 
     afterEach(() => {
-        // vi.useRealTimers();
+        vi.useRealTimers();
     });
 
     test("Should be able to validate the check-in", async () => {
@@ -39,5 +40,22 @@ describe("Validate Check-in Service", () => {
                 checkInId: "nonexistent-check-in-id"
             })
         ).rejects.toBeInstanceOf(ResourceNotFoundError);
+    });
+
+    test("Should not be able to validate check-in after 20 minutes of its creation", async () => {
+        vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+        const createdCheckIn = await checkInsRepository.create({
+            gym_id: "gym 01",
+            user_id: "user-01"
+        });
+
+        const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+        vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+        await expect(() => sut.execute({
+            checkInId: createdCheckIn.id
+        })).rejects.toBeInstanceOf(LateCheckInValidationError);
     });
 });
