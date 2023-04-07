7d52381df67a92f875d7b410323f3682d899fefd
Vinícius <viniscrv@gmail.com>
2023-04-07 14:28:11 -0300

    feat: implementado JWT no fastify

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,8 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function profile(req: FastifyRequest, res: FastifyReply) {
+
+    await req.jwtVerify();
+
+    return res.status(200).send();
+}
