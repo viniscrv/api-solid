a6d2af270eb8058bcb1c5812caf04d121d800ed6
Vinícius <viniscrv@gmail.com>
2023-04-08 14:26:21 -0300

    test: teste e2e de registro de usuário

--- src/http/controllers/register.test.ts
+++ src/http/controllers/register.test.ts
@@ -1,5 +1,25 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {
+describe("Register (2e2)", () => {
 
+    beforeAll(async () => {
+        await app.ready();
+    });
+    afterAll(async () => {
+        await app.close();
+    });
+
+    test("should be able to register", async () => {
+        const response = await request(app.server)
+            .post("/users")
+            .send({
+                name: "John Doe",
+                email: "johndoe@exemple.com",
+                password: "123456"
+            });
+
+        expect(response.statusCode).toEqual(201);
+    });
 });
